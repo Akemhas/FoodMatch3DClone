ac076ab0d23766c510884c3442cdb67cc3dc36ae
Akif Emre Bozdemir <61284057+Akemhas@users.noreply.github.com>
2023-04-16 15:51:19 +0300

    [Added] FillBar

---
+++ Assets/Scripts/GamePlay/FillBar.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class FillBar : MonoBehaviour
+{
+    [SerializeField] private Image fillImage;
+    [SerializeField] private float fillDuration = .15f;
+
+    private Sequence fillSequence;
+
+    // Fill amount is between 0 and 1
+    public void FillTheBar(float fillAmount, float fillExpireDuration)
+    {
+        fillSequence?.Kill();
+        fillSequence = DOTween.Sequence();
+        fillSequence.Append(fillImage.DOFillAmount(fillAmount, fillDuration).SetEase(Ease.Linear));
+        if (fillExpireDuration == -1) return;
+        fillSequence.Append(fillImage.DOFillAmount(0, fillExpireDuration).SetEase(Ease.Linear));
+    }
+}
--- Assets/Scripts/GamePlay/ItemSlotManager.cs
+++ Assets/Scripts/GamePlay/ItemSlotManager.cs
@@ -6,10 +6,12 @@ public class ItemSlotManager : Singleton<ItemSlotManager>
 {
     public ItemSlot itemSlot => _itemSlots[0];
     [SerializeField] private ItemSlot[] _itemSlots;
+    [SerializeField] private Transform seperatorCollider;
 
     [SerializeField][Range(0, 12)] private float _slotScale = 8;
     [SerializeField][Range(0, 1)] private float _slotsYPosition = .15f;
     [SerializeField][Range(0, .5f)] private float _padding = .25f;
+    [SerializeField] private float zOffsetFromCamera = 11;
 
     private void OnValidate()
     {
@@ -20,9 +22,10 @@ public class ItemSlotManager : Singleton<ItemSlotManager>
         float spacing = (1 - _padding) / Mathf.Clamp(_itemSlots.Length - 1, 0, float.MaxValue);
         for (int i = 0; i < length; i++)
         {
-            Vector3 pos = new(_padding * .5f + spacing * i, _slotsYPosition, 10);
+            Vector3 pos = new(_padding * .5f + spacing * i, _slotsYPosition, zOffsetFromCamera);
             _itemSlots[i].transform.position = cam.ViewportToWorldPoint(pos);
-            _itemSlots[i].model.localScale = Vector3.one * _slotScale / 10;
+            _itemSlots[i].model.localScale = Vector3.one * _slotScale / zOffsetFromCamera;
         }
+        seperatorCollider.position = cam.ViewportToWorldPoint(new Vector3(0.5f, _slotsYPosition + 0.1f, zOffsetFromCamera));
     }
 }
